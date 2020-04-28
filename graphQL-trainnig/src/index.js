50dcd492b73121b8b1d7af7384b9372baef58018
Igor Satek <igorsatek@gmail.com>
2020-04-28 22:16:45 +0200

    Simple queries complete, begining of realations

--- graphQL-trainnig/src/index.js
+++ graphQL-trainnig/src/index.js
@@ -1,10 +1,46 @@
 import { GraphQLServer } from 'graphql-yoga'
+
+const users = [{
+  id: "User2588754",
+  name: "Igor Šátek",
+  email: "igorsatek@gmail.com",
+  age: 27
+},{
+  id: "User2288759",
+  name: "Martin Kuchto",
+  email: "kuchto@gmail.com",
+},{
+  id: "User2386754",
+  name: "Roman Pinček",
+  email: "romanpincek@gmail.com",
+  age: 27
+}]
+
+const posts = [{
+  id: "Post655897",
+  title: "Najkrajší deň",
+  body: "Včera som zažila tie najkrajši narodeniny môj syn mi...",
+  published: true,
+  author: "User2588754"
+},{
+  id: "Post653897",
+  title: "Najhorší deň",
+  body: "Včera som zažila ten najhorší deň môj muž mi...",
+  published: true,
+  author: "User2588754"
+},{
+  id: "Post654897",
+  title: "Deň za dňom mi",
+  body: "Každý deň je rovnaký neviem ako dlho to ešte zvládnem...",
+  published: false,
+  author: "User2288759"
+}]
 // Type definition => Schema
 // Scaler Types: String, Boolean, Int, Float, ID
 const typeDefs = `
 type Query {
-  greeting(name: String, position: String): String!
-  add(a: Float!, b: Float!): Float!
+  users(query: String): [User!]!
+  posts(query: String): [Post!]!
   me: User!
   post: Post!
 }
@@ -19,20 +55,28 @@ type Post {
   title: String!
   body: String!
   published: Boolean!
+  author: User!
 }
 `
 //Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info){
-      if(args.name && args.position) {
-        return `helloooo, ${args.name}! ty si môj oblúbený ${args.position}`
-      } else {
-        return "helo pan anonim :)"
-      } 
+    users(parent, args, ctx, info) {
+      if(!args.query) {
+        return users
+      }
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
     },
-    add(parent, args, ctx, info) {
-      return args.a + args.b
+    posts(parent, args, ctx, info) {
+      if(!args.query) {
+        return posts
+      }
+      return posts.filter((post) => {
+        return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+      })
+
     },
    me() {
      return {
@@ -50,6 +94,14 @@ const resolvers = {
        published: false
      }
    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    }
+
   }
 
 }
